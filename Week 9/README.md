178049d259bf6f3486192e96166f50f1139af2fa
joegreene <joeisgreene@csu.fullerton.edu>
2014-03-17 14:07:07 -0700

    Week 9 Initialized
    Added solution for guessing game and rough draft of README

---
+++ Week 9/README.md
@@ -0,0 +1,40 @@
+# Week 9
+
+## Contents
+- [Solutions (From Last Week)](#solutions-from-last-week)
+- [Intro](#intro)
+- [Chapter 2](#chapter-2)
+  - [Operators (General)](#operators-(general))
+- [Chapter 3](#chapter-3)
+  - [iomanip](#iomanip)
+- [Chapter 4](#chapter-4)
+  - [if/else statements](#if-else)
+  - [switch](#switch)
+  - [enum](#enum)
+  - [conditional operator](#conditional-operator)
+- [Miscellaneous](#miscellaneous)
+  - [Truth Tables](#truth-tables)
+- [More Practice](#class-practice)
+  - [Separate Exercise](#separate-exercise)
+- [Extra Challenges](#extra-challenges)
+
+### Solutions (From Last Week)
+In the usual spot (under files location) above. If there are any issues with it, let me (the SI instructor) know. It's not necessarily a robust copy, but it serves as 
+an example of how to scan for a decimal value, round, and so forth.
+
+### Intro
+This week's work is to review all topic I either thought might have been troublesome, or 
+
+If you feel comfortable with the material after reading the notes, I've attached exercises to work on as well.
+
+### Chapter 3
+
+
+### Chapter 4
+
+
+-------------------------------------------------------------------------------
+Copyright &copy; 2014 Joseph Greene <joeisgreene@csu.fullerton.edu>
+
+Released under [The MIT License] (http://opensource.org/licenses/MIT)  
+Project located at <https://github.com/joegreene/Spring-2014-CS-Lab--SI-/>
---
+++ Week 9/week8_guessing_game_remade.cpp
@@ -0,0 +1,63 @@
+// week_8_guessing_game_remade.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  srand(time(NULL)); //generate new seed for random number generator
+
+  bool finished = false; //to determine when to stop the loop
+  const int MAX = 10; //must initialize when using constant variables
+  const int LOWER_LIMIT = 1; //if you'd like to make your program more 
+                             //customizable, make a lower limit variable
+  const int RAND_NUM = rand() % MAX + LOWER_LIMIT; //static random number
+                                                   //(static == doesn't change)
+  const int QUIT_VALUE = 55;
+  int guess = 0; //"  " guess
+  int num_tries = 0; //number of guesses
+
+  cout << "Welcome to the random number guessing game!" 
+       << endl; //prompt
+  cout << "If you would like to quit at any time, enter in " << QUIT_VALUE 
+       << " when guessing." << endl; //more prompt
+
+  do
+  {
+    cout << "Enter in a value between " << LOWER_LIMIT << " and " << MAX << endl;
+    cin >> guess;
+
+    if(guess == QUIT_VALUE) //if user decides to quit
+      finished = true;
+    else if (guess > MAX || guess < LOWER_LIMIT) // if guess is out of bounds
+    {
+      cout << "Your guess was out of bounds. Enter values between "
+           << LOWER_LIMIT << " and " << MAX << endl;
+      num_tries++;
+    }
+    else if (guess == RAND_NUM)
+    {
+      cout << "You guessed it right! Good job!" << endl;
+      finished = true;
+      num_tries++;
+    }
+    else
+    {
+      cout << "Sorry, but you guessed incorrectly. Try again." << endl;
+      num_tries++;
+    }
+  }while(!finished); //while the user isn't finished
+
+  cout << "Actual random number value: " << RAND_NUM << endl;
+  cout << "Number of guesses made: " << num_tries << endl;
+
+  cout << "Thanks for playing!" << endl;
+
+	return 0;
+}
+
